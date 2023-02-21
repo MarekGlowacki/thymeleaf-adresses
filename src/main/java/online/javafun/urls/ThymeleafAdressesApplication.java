2990d8d155382290d606defcd6237917d954ff3e
User <user@its.me>
2023-02-21 10:20:08 +0100

    Initial commit

---
+++ src/main/java/online/javafun/urls/ThymeleafAdressesApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.urls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafAdressesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafAdressesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/javafun/urls/ThymeleafAdressesApplicationTests.java
@@ -0,0 +1,10 @@
+package online.javafun.urls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafAdressesApplicationTests {
+
+    @Test
+    void contextLoads() {
